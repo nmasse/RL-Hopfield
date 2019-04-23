3971803ed0cab0e44bfb4659bdb4bf45d9c52b11
gdgrant <gdgrant@uchicago.edu>
2019-04-23 13:23:48 -0500

    Updating parameter name

--- atari_parameters.py
+++ atari_parameters.py
@@ -51,7 +51,7 @@ par = {
 	# Task specs
 	'gym_env'				: 'SpaceInvaders-v0',
 	'task'					: 'atari',
-	'frames_per_iter'		: 2500000000,
+	'num_frames'			: 2500000000,
 	'k_skip'				: 4,
 	'rewards'				: [-1.,1.],
 	'num_actions'			: 4,		# The number of different actions available to the agent
